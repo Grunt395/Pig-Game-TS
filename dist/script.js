33c0bf12b365f11f9d50b3488881d5eda4f7c204
Grant <grantlew@att.net>
2026-02-10 12:29:05 -0800

    Created Useful Variables

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,22 @@
-const randNum = Math.floor((Math.random() * 6) + 1);
-const rollDice = document.querySelector(".btn-roll");
-if (!rollDice) {
+const diceElement = document.querySelector(".dice");
+const p0ScoreElement = document.querySelector(".score--0");
+const p1ScoreElement = document.querySelector(".score--1");
+const p0Current = document.querySelector(".current--0");
+const p1Current = document.querySelector(".current--1");
+const rollButton = document.querySelector(".btn--roll");
+const holdButton = document.querySelector(".btn--hold");
+const newButton = document.querySelector(".btn--new");
+if (!rollButton || !diceElement || !holdButton || !newButton || !p0ScoreElement || !p1ScoreElement || !p0Current || !p1Current) {
     throw "Error - null value";
 }
-rollDice.addEventListener("click", function () {
+let p0Score = 0;
+let p1Score = 0;
+p0ScoreElement.textContent = p0Score.toString();
+p1ScoreElement.textContent = p1Score.toString();
+diceElement.classList.add("hidden");
+rollButton.addEventListener("click", function () {
+    const randNum = Math.floor((Math.random() * 6) + 1);
+    diceElement.classList.remove("hidden");
+    diceElement.src = `./dice/dice-${randNum}.png`;
 });
 export {};
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,29 @@
-const randNum: number = Math.floor((Math.random() * 6) + 1);
+const diceElement = document.querySelector<HTMLImageElement>(".dice");
+const p0ScoreElement = document.querySelector<HTMLParagraphElement>(".score--0");
+const p1ScoreElement = document.querySelector<HTMLParagraphElement>(".score--1");
+const p0Current = document.querySelector<HTMLParagraphElement>(".current--0");
+const p1Current = document.querySelector<HTMLParagraphElement>(".current--1");
+const rollButton = document.querySelector(".btn--roll");
+const holdButton = document.querySelector(".btn--hold");
+const newButton = document.querySelector(".btn--new");
 
-const rollDice = document.querySelector(".btn-roll");
-
-if (!rollDice) {
+if (!rollButton || !diceElement || !holdButton || !newButton || !p0ScoreElement || !p1ScoreElement || !p0Current || !p1Current) {
     throw "Error - null value";
 }
 
-rollDice.addEventListener("click", function () {
+let p0Score: number = 0;
+let p1Score: number = 0;
+
+p0ScoreElement.textContent = p0Score.toString();
+p1ScoreElement.textContent = p1Score.toString();
+
+diceElement.classList.add("hidden");
+
+rollButton.addEventListener("click", function () {
+    const randNum: number = Math.floor((Math.random() * 6) + 1);
     
+    diceElement.classList.remove("hidden");
+    diceElement.src = `./dice/dice-${randNum}.png`;
+
 });
+
