ba0d0cbaf21a45dd781186fbac06e7d5d1083f57
Grant <grantlew@att.net>
2026-02-23 20:34:27 -0800

    Implemented Active Player Functionality

--- dist/script.js
+++ dist/script.js
@@ -1,88 +1,66 @@
-const diceElement = document.querySelector(".dice");
-const p0ScoreElement = document.querySelector(".score--0");
-const p1ScoreElement = document.querySelector(".score--1");
-const p0CurrentElement = document.querySelector(".current--0");
-const p1CurrentElement = document.querySelector(".current--1");
-const player0Element = document.querySelector(".player--0");
-const player1Element = document.querySelector(".player--1");
-const rollButton = document.querySelector(".btn--roll");
-const holdButton = document.querySelector(".btn--hold");
-const newButton = document.querySelector(".btn--new");
-let p0Score = 0;
-let p1Score = 0;
-let p0Current = 0;
-let p1Current = 0;
-if (!rollButton || !diceElement || !holdButton || !newButton || !p0ScoreElement || !p1ScoreElement || !p0CurrentElement || !p1CurrentElement || !player0Element || !player1Element) {
-    throw "Error - null value";
+function select(input) {
+    const elem = document.querySelector(input);
+    if (!elem) {
+        throw `Error - ${input} is a null element`;
+    }
+    return elem;
 }
-p0ScoreElement.textContent = p0Score.toString();
-p1ScoreElement.textContent = p1Score.toString();
-diceElement.classList.add("hidden");
+const p0TempElement = select(".temp--0");
+const p1TempElement = select(".temp--1");
+const players = Array.from(document.querySelectorAll(".player"));
+const diceElement = select(".dice");
+const rollButton = select(".btn--roll");
+const holdButton = select(".btn--hold");
+const newButton = select(".btn--new");
+let activePlayer = 0;
+let tempScore = 0;
+let savedScore = [0, 0];
+function swapPlayer() {
+    activePlayer = activePlayer ? 0 : 1;
+    players.forEach((elem) => elem.classList.toggle("player--active"));
+}
+p0TempElement.textContent = tempScore.toString();
+p1TempElement.textContent = tempScore.toString();
 rollButton.addEventListener("click", function () {
     const randNum = Math.floor((Math.random() * 6) + 1);
     diceElement.src = `./dice/dice-${randNum}.png`;
-    diceElement.classList.remove("hidden");
-    if (player0Element.classList.contains("player--active")) {
-        if (randNum != 1) {
-            p0Score += randNum;
-            p0ScoreElement.textContent = p0Score.toString();
-        }
-        else {
-            p0Score = 0;
-            p0ScoreElement.textContent = p0Score.toString();
-            player0Element.classList.remove("player--active");
-            player1Element.classList.add("player--active");
-        }
+    diceElement.hidden = false;
+    if (randNum != 1) {
+        tempScore += randNum;
+        select(`.temp--${activePlayer}`).textContent = tempScore.toString();
     }
     else {
-        if (randNum != 1) {
-            p1Score += randNum;
-            p1ScoreElement.textContent = p1Score.toString();
-        }
-        else {
-            p1Score = 0;
-            p1ScoreElement.textContent = p1Score.toString();
-            player0Element.classList.add("player--active");
-            player1Element.classList.remove("player--active");
-        }
+        tempScore = 0;
+        select(`.temp--${activePlayer}`).textContent = tempScore.toString();
+        swapPlayer();
     }
 });
 holdButton.addEventListener("click", function () {
-    if (player0Element.classList.contains("player--active")) {
-        p0Current += p0Score;
-        p0Score = 0;
-        p0CurrentElement.textContent = p0Current.toString();
-        p0ScoreElement.textContent = p0Score.toString();
-        player0Element.classList.remove("player--active");
-        player1Element.classList.add("player--active");
+    savedScore[activePlayer] += tempScore;
+    select(`.score--${activePlayer}`).textContent = savedScore[activePlayer].toString();
+    tempScore = 0;
+    select(`.temp--${activePlayer}`).textContent = tempScore.toString();
+    if (savedScore[activePlayer] >= 100) {
+        players[activePlayer].classList.remove("player--active");
+        players[activePlayer].classList.add("player--winner");
+        rollButton.disabled = true;
+        holdButton.disabled = true;
     }
     else {
-        p1Current += p1Score;
-        p1Score = 0;
-        p1CurrentElement.textContent = p1Current.toString();
-        p1ScoreElement.textContent = p1Score.toString();
-        player0Element.classList.add("player--active");
-        player1Element.classList.remove("player--active");
-    }
-    if (p0Current >= 100) {
-        player0Element.classList.add("player--winner");
-        player0Element.classList.remove("player--active");
-    }
-    else if (p1Current >= 100) {
-        player1Element.classList.add("player--winner");
-        player1Element.classList.remove("player--active");
+        swapPlayer();
     }
 });
 newButton.addEventListener("click", function () {
-    p0Score = 0;
-    p1Score = 0;
-    p0Current = 0;
-    p1Current = 0;
-    p0CurrentElement.textContent = p0Current.toString();
-    p1CurrentElement.textContent = p0Current.toString();
-    p0ScoreElement.textContent = p0Score.toString();
-    p1ScoreElement.textContent = p0Score.toString();
-    player0Element.classList.add("player--active");
-    player1Element.classList.remove("player--active");
+    tempScore = 0;
+    savedScore = [0, 0];
+    activePlayer = 0;
+    p0TempElement.textContent = "0";
+    p1TempElement.textContent = "0";
+    select(".score--0").textContent = savedScore[0].toString();
+    select(".score--1").textContent = savedScore[1].toString();
+    players.forEach(elem => elem.classList.remove("player--winner", "player--active"));
+    players[0].classList.add("player--active");
+    rollButton.disabled = false;
+    holdButton.disabled = false;
 });
 export {};
--- src/script.ts
+++ src/script.ts
@@ -1,103 +1,89 @@
-const diceElement = document.querySelector<HTMLImageElement>(".dice");
-const p0ScoreElement = document.querySelector<HTMLParagraphElement>(".score--0");
-const p1ScoreElement = document.querySelector<HTMLParagraphElement>(".score--1");
-const p0CurrentElement = document.querySelector<HTMLParagraphElement>(".current--0");
-const p1CurrentElement = document.querySelector<HTMLParagraphElement>(".current--1");
-const player0Element = document.querySelector(".player--0");
-const player1Element = document.querySelector(".player--1");
-const rollButton = document.querySelector(".btn--roll");
-const holdButton = document.querySelector(".btn--hold");
-const newButton = document.querySelector(".btn--new");
+// QuerySelector Function for Null Values
+function select (input: string) {
+    const elem = document.querySelector(input);
+    if (!elem) {
+        throw `Error - ${input} is a null element`;
+    }
+    return elem;
+}
 
-let p0Score: number = 0;
-let p1Score: number = 0;
-let p0Current: number = 0;
-let p1Current: number = 0;
+// Element Variables
+const p0TempElement = select(".temp--0");
+const p1TempElement = select(".temp--1");
+const players = Array.from(document.querySelectorAll(".player"));
 
-if (!rollButton || !diceElement || !holdButton || !newButton || !p0ScoreElement || !p1ScoreElement || !p0CurrentElement || !p1CurrentElement || !player0Element || !player1Element) {
-    throw "Error - null value";
+const diceElement = select(".dice") as HTMLImageElement;
+const rollButton = select(".btn--roll") as HTMLButtonElement;
+const holdButton = select(".btn--hold") as HTMLButtonElement;
+const newButton = select(".btn--new") as HTMLButtonElement;
+
+// Numeric Variables
+let activePlayer: 0 | 1 = 0;
+let tempScore: number = 0;
+let savedScore: number[] = [0, 0];
+
+// Swap Player Function
+function swapPlayer() {
+    activePlayer = activePlayer ? 0 : 1;
+    players.forEach((elem) => elem.classList.toggle("player--active"));
 }
 
 // Initial State Changes
-p0ScoreElement.textContent = p0Score.toString();
-p1ScoreElement.textContent = p1Score.toString();
-diceElement.classList.add("hidden");
+p0TempElement.textContent = tempScore.toString();
+p1TempElement.textContent = tempScore.toString();
 
 // Roll Dice Button
 rollButton.addEventListener("click", function () {
     const randNum: number = Math.floor((Math.random() * 6) + 1);
     
     diceElement.src = `./dice/dice-${randNum}.png`;
-    diceElement.classList.remove("hidden");
+    diceElement.hidden = false;
 
-    if (player0Element.classList.contains("player--active")) {
-        if (randNum != 1) {
-            p0Score += randNum;
-            p0ScoreElement.textContent = p0Score.toString();
-        }
-        else {
-            p0Score = 0;
-            p0ScoreElement.textContent = p0Score.toString();
-            player0Element.classList.remove("player--active");
-            player1Element.classList.add("player--active");
-        }
+    if (randNum != 1) {
+        tempScore += randNum;
+        select(`.temp--${activePlayer}`).textContent = tempScore.toString();
     }
 
     else {
-        if (randNum != 1) {
-            p1Score += randNum;
-            p1ScoreElement.textContent = p1Score.toString();
-        }
-        else {
-            p1Score = 0;
-            p1ScoreElement.textContent = p1Score.toString();
-            player0Element.classList.add("player--active");
-            player1Element.classList.remove("player--active");
-        }
+        tempScore = 0;
+        select(`.temp--${activePlayer}`).textContent = tempScore.toString();
+        swapPlayer();
     }
+
 });
 
 // Hold Score Button
 holdButton.addEventListener("click", function () {
-    if (player0Element.classList.contains("player--active")) {
-        p0Current += p0Score;
-        p0Score = 0;
-        p0CurrentElement.textContent = p0Current.toString();
-        p0ScoreElement.textContent = p0Score.toString();
-        player0Element.classList.remove("player--active");
-        player1Element.classList.add("player--active");
+
+    savedScore[activePlayer] += tempScore;
+    select(`.score--${activePlayer}`).textContent = savedScore[activePlayer].toString();
+
+    tempScore = 0;
+    select(`.temp--${activePlayer}`).textContent = tempScore.toString();
+
+    if (savedScore[activePlayer] >= 100) {
+        players[activePlayer].classList.remove("player--active");
+        players[activePlayer].classList.add("player--winner");
+        rollButton.disabled = true;
+        holdButton.disabled = true;
     }
 
     else {
-        p1Current += p1Score;
-        p1Score = 0;
-        p1CurrentElement.textContent = p1Current.toString();
-        p1ScoreElement.textContent = p1Score.toString();
-        player0Element.classList.add("player--active");
-        player1Element.classList.remove("player--active");
-    }
-
-    // Check For Winner
-    if (p0Current >= 100) {
-        player0Element.classList.add("player--winner");
-        player0Element.classList.remove("player--active");
-    }
-    else if (p1Current >= 100) {
-        player1Element.classList.add("player--winner");
-        player1Element.classList.remove("player--active");
+        swapPlayer();
     }
 });
 
 // New Game Button
 newButton.addEventListener("click", function () {
-    p0Score = 0;
-    p1Score = 0;
-    p0Current = 0;
-    p1Current = 0;
-    p0CurrentElement.textContent = p0Current.toString();
-    p1CurrentElement.textContent = p0Current.toString();
-    p0ScoreElement.textContent = p0Score.toString();
-    p1ScoreElement.textContent = p0Score.toString();
-    player0Element.classList.add("player--active");
-    player1Element.classList.remove("player--active");
+    tempScore = 0;
+    savedScore = [0, 0];
+    activePlayer = 0;
+    p0TempElement.textContent = "0";
+    p1TempElement.textContent = "0";
+    select(".score--0").textContent = savedScore[0].toString();
+    select(".score--1").textContent = savedScore[1].toString();
+    players.forEach(elem => elem.classList.remove("player--winner", "player--active"));
+    players[0].classList.add("player--active");
+    rollButton.disabled = false;
+    holdButton.disabled = false;
 });
